5c3620f907b365947ed245f11da3b00e576def14
seoyeon0619 <127061717+seoyeon0619@users.noreply.github.com>
2023-03-21 15:15:47 +0900

    Delete myapp33 directory

--- myapp33/src/main/java/com/kosa/myapp3/common/CommonConst.java
+++
@@ -1,7 +0,0 @@
-package com.kosa.myapp3.common;
-
-//공통 상수 클래스
-public class CommonConst {
-	public final static String UPLOADPATH=
-	"C:\\web_programming\\spring_workspace1\\myapp3\\src\\main\\webapp\\upload";
-}
